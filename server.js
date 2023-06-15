7b5f68caa42d6e093c9bfff6c93a7a1db264c5f6
Greg <greg19850@gmail.com>
2023-06-15 12:53:48 +0100

    Added main endpoint, added process.env port

--- server.js
+++ server.js
@@ -8,12 +8,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req,res) => {res.send('Server response OK')})
 app.get('/profile/:id', getUser);
 app.post('/signin', signInUser);
 app.post('/register', registerUser);
 app.put('/image', addImageStats);
 
-const PORT = 3001;
+const { PORT = 9090} = process.env;
 
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
