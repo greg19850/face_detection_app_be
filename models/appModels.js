dfde457a9e96b8878aea5ef85a332835c63d090a
Greg <greg19850@gmail.com>
2023-06-21 19:10:15 +0100

    knex connection

--- models/appModels.js
+++ models/appModels.js
@@ -11,7 +11,7 @@ require('dotenv').config({
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL
   },
   pool: {min:0, max: 2}
 })
